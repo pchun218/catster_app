66494b5a6acf2ef0ba996ef40a9b18d053830a21
pchun218 <pchun218@gmail.com>
2015-09-30 19:26:16 -0700

    updating homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :signups
+  resources :cat_gifs
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
